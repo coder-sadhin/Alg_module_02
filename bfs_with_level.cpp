448c6c168d0940c1b0ef2e14d56fc05aea5bd2b4
coder-sadhin <codersadhin@gmail.com>
2024-09-02 20:25:45 +0600

    bfs function

--- bfs_with_level.cpp
+++ bfs_with_level.cpp
@@ -3,3 +3,24 @@ using namespace std;
 vector<int> v[1005];
 bool vis[1005];
 int level[1005];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    level[src] = 0;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        for (int child : v[par])
+        {
+            if (vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[par] + 1;
+            }
+        }
+    }
+}
