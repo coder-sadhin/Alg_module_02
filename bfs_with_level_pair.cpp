f0eac280151b8d71d1542230003787ea9e66e80c
coder-sadhin <codersadhin@gmail.com>
2024-09-02 20:27:08 +0600

    bfs function

--- bfs_with_level_pair.cpp
+++ bfs_with_level_pair.cpp
@@ -2,3 +2,35 @@
 using namespace std;
 vector<int> v[1005];
 bool vis[1005];
+void bfs(int src, int des)
+{
+    queue<pair<int, int>> q;
+    q.push({src, 0});
+    vis[src] = true;
+    bool paisi = false;
+    while (!q.empty())
+    {
+        pair<int, int> p = q.front();
+        q.pop();
+        int par = p.first;
+        int level = p.second;
+        // cout << par << endl;
+        if (par == des)
+        {
+            cout << level << endl;
+            paisi = true;
+        }
+        for (int child : v[par])
+        {
+            if (vis[child] == false)
+            {
+                q.push({child, level + 1});
+                vis[child] = true;
+            }
+        }
+    }
+    if (paisi == false)
+    {
+        cout << -1 << endl;
+    }
+}
