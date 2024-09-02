cd4b7181f10e49ccf84dbadb4fa85cae66e15516
coder-sadhin <codersadhin@gmail.com>
2024-09-02 20:24:27 +0600

    bfs function

--- bfs_traversal.cpp
+++ bfs_traversal.cpp
@@ -2,3 +2,23 @@
 using namespace std;
 vector<int> v[1005];
 bool vis[1005];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        cout << par << endl;
+        for (int child : v[par])
+        {
+            if (vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }
+}
