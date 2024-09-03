a4c4b1e01f398febaa5525ec2b3af1be0a1a9a97
coder-sadhin <codersadhin@gmail.com>
2024-09-03 20:28:59 +0600

    bfs function

--- path_printing.cpp
+++ path_printing.cpp
@@ -4,3 +4,25 @@ vector<int> v[1005];
 bool vis[1005];
 int level[1005];
 int parent[1005];
+void bfs(int src)
+{
+    queue<int> q;
+    q.push(src);
+    vis[src] = true;
+    level[src] = 0;
+    while (!q.empty())
+    {
+        int par = q.front();
+        q.pop();
+        for (int child : v[par])
+        {
+            if (vis[child] == false)
+            {
+                q.push(child);
+                vis[child] = true;
+                level[child] = level[par] + 1;
+                parent[child] = par;
+            }
+        }
+    }
+}
