54e5307be81409a5deff13bbc07fff83ff255287
coder-sadhin <codersadhin@gmail.com>
2024-09-03 20:29:35 +0600

    push on vector

--- path_printing.cpp
+++ path_printing.cpp
@@ -26,3 +26,30 @@ void bfs(int src)
         }
     }
 }
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+    int src, des;
+    cin >> src >> des;
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    memset(parent, -1, sizeof(parent));
+    bfs(src);
+    int x = des;
+    vector<int> path;
+    while (x != -1)
+    {
+        path.push_back(x);
+        x = parent[x];
+    }
+   
+    return 0;
+}
